ea3ac1155fe7b60b8480b370d7a24335f25325f7
Laika-Beats <laikabeatsdenver@gmail.com>
2020-11-25 12:19:14 -0700

    id, key, and boolean

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Todo( {todo} ) {
+    return (
+        <div>
+            <label>
+                {todo.name}
+            </label>
+            
+            
+        </div>
+    )
+}
