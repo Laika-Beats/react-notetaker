558ba3fd02a259c9c71e664b2e4b03e720ba4423
Laika-Beats <laikabeatsdenver@gmail.com>
2020-11-25 12:32:21 -0700

    local storage function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from "./TodoList"
+import uuidv4 from "uuid/v4"
+
+const localStorage = "todoApp.todos"
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  useEffect(() => {
+    localStorage.setItem(localStorage, JSON.stringify(todos))
+  }, [todos])
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === "") return
     console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, cmomplete: false}]
+    })
+    todoNameRef.current.value = null
 
   }
+
   return (
     < >
     <TodoList todos = {todos}/>
